d30f4b5fdaf725fc8a755cc648e181e14c9d4258
Aleksandr Jevlentjev <jevlentjev@gmail.com>
2019-09-10 17:28:12 +0300

    some more components and styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
+import Navigation from './components/Navigation/Navigation.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "white",
+        blur: 5
+      }
+    },
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* 
         
